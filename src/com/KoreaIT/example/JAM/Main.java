e19bc82558d571920979b01c8c972b6705de7890
Inha921119 <dlsgk921119@gmail.com>
2023-03-08 15:13:00 +0900

    article list시 DB에 있는 데이터 리스팅

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -3,6 +3,7 @@ package com.KoreaIT.example.JAM;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,10 +15,6 @@ public class Main {
 		
 		System.out.println("== 프로그램 시작 ==");
 		
-		List<Article> articles = new ArrayList<>();
-		
-		int lastArticleId = 0;
-		
 		while (true) {
 			System.out.printf("명령어) ");
 			String cmd = sc.nextLine().trim();
@@ -28,8 +25,6 @@ public class Main {
 			
 			if (cmd.equals("article write")) {
 				System.out.println("== 게시물 작성 ==");
-				
-				int id = lastArticleId + 1;
 				System.out.printf("제목 : ");
 				String title = sc.nextLine().trim();
 				System.out.printf("내용 : ");
@@ -37,21 +32,80 @@ public class Main {
 				
 				doWrite(title, body);
 
-				System.out.printf("%d번 글이 생성되었습니다.\n", id);
+				System.out.printf("?번 글이 생성되었습니다.\n");
 				continue;
 		} else if (cmd.equals("article list")) {
 			System.out.println("== 게시물 목록 ==");
+			
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			
+			List<Article> articles = new ArrayList<>();
+
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				String url = "jdbc:mysql://127.0.0.1:3306/jdbc_article_manager?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+				conn = DriverManager.getConnection(url, "root", "");
+				
+				String sql = "SELECT *";
+				sql += " FROM article";
+				sql += " ORDER BY id DESC;";
+				
+				pstmt = conn.prepareStatement(sql);
+				rs = pstmt.executeQuery();
+				
+				while(rs.next()) {
+					int id = rs.getInt("id");
+					String regDate = rs.getString("regDate");
+					String updateDate = rs.getString("updateDate");
+					String title = rs.getString("title");
+					String body = rs.getString("body");
+					
+					Article article = new Article(id, regDate, updateDate, title, body);
+					articles.add(article);
+					
+				}
+				
+			} catch (ClassNotFoundException e) {
+				System.out.println("드라이버 로딩 실패");
+			} catch (SQLException e) {
+				System.out.println("에러: " + e);
+			} finally {
+				try {
+					if (rs != null && !rs.isClosed()) {
+						rs.close();
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				try {
+					if (pstmt != null && !pstmt.isClosed()) {
+						pstmt.close();
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				try {
+					if (conn != null && !conn.isClosed()) {
+						conn.close();
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 			
 			if(articles.size() == 0) {
 				System.out.println("게시물이 없습니다.");
 				continue;
 			}
 			
-			System.out.println("번호		|		제목");
+			System.out.println("번호	|제목");
 			for (Article article : articles) {
-				System.out.printf("%d		|		%s\n", article.id, article.title);
+				System.out.printf("%d	|%s\n", article.id, article.title);
 			}
-		}else {
+		} else {
 			System.out.println("명령어가 없습니다.");
 		}
 			

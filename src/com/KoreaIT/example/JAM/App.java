33758fade02f21f83d756083e54584f49c5c8662
Inha921119 <dlsgk921119@gmail.com>
2023-03-13 14:46:22 +0900

    member join 기능 구현

--- src/com/KoreaIT/example/JAM/App.java
+++ src/com/KoreaIT/example/JAM/App.java
@@ -33,9 +33,9 @@ public class App {
 					System.out.println("== 게시물 작성 ==");
 
 					System.out.printf("제목 : ");
-					String title = sc.nextLine();
+					String title = sc.nextLine().trim();
 					System.out.printf("내용 : ");
-					String body = sc.nextLine();
+					String body = sc.nextLine().trim();
 
 					SecSql sql = new SecSql();
 
@@ -94,9 +94,9 @@ public class App {
 					System.out.println("== 게시물 수정 ==");
 					
 					System.out.printf("수정할 제목 : ");
-					String title = sc.nextLine();
+					String title = sc.nextLine().trim();
 					System.out.printf("수정할 내용 : ");
-					String body = sc.nextLine();
+					String body = sc.nextLine().trim();
 
 					sql = new SecSql();
 
@@ -158,15 +158,88 @@ public class App {
 					
 					System.out.printf("%d번 글이 삭제되었습니다\n", id);
 				} else if (cmd.equals("member join")) {
+					String loginId = null;
+					String loginPw = null;
+					String loginPwChk = null;
+					String name = null;
+					String phoneNum = null;
+					System.out.println("== 회원 가입 ==");
 					
+					while (true) {
+						System.out.printf("아이디 : ");
+						loginId = sc.nextLine().trim();
+						
+						if (loginId.length() == 0) {
+							System.out.println("아이디를 입력해주세요");
+							continue;
+						}
+						break;
+					}
+					
+					while (true) {
+						System.out.printf("비밀번호 : ");
+						loginPw = sc.nextLine().trim();
+						
+						if (loginPw.length() == 0) {
+							System.out.println("비밀번호를 입력해주세요");
+							continue;
+						}
+						
+						boolean loginPwCheck = true;
+						
+						while (true) {
+							System.out.printf("비밀번호 확인 : ");
+							loginPwChk = sc.nextLine().trim();
+							
+							if (loginPwChk.length() == 0) {
+								System.out.println("비밀번호 확인을 입력해주세요");
+								continue;
+							}
+							
+							if (loginPw.equals(loginPwChk) == false) {
+								System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
+								loginPwCheck = false;
+							}
+							break;
+						}
+						if (loginPwCheck) {
+							break;
+						}
+					}
+					while (true) {
+						System.out.printf("이름 : ");
+						name = sc.nextLine().trim();
+						
+						if (name.length() == 0) {
+							System.out.println("이름을 입력해주세요");
+							continue;
+						}
+						break;
+					}
+					while (true) {
+						System.out.printf("전화번호 : ");
+						phoneNum = sc.nextLine().trim();
+						
+						if (phoneNum.length() == 0) {
+							System.out.println("전화번호를 입력해주세요");
+							continue;
+						}
+						break;
+					}
+
 					SecSql sql = new SecSql();
 
-					sql.append("DELETE FROM article");
-					sql.append("WHERE id = ?");
+					sql.append("INSERT INTO member");
+					sql.append("SET regDate = NOW()");
+					sql.append(", updateDate = NOW()");
+					sql.append(", loginId = ?", loginId);
+					sql.append(", loginPw = ?", loginPw);
+					sql.append(", `name` = ?", name);
+					sql.append(", phoneNum = ?", phoneNum);
 
-					DBUtil.delete(conn, sql);
-					
-					System.out.printf("%d번 글이 삭제되었습니다\n");
+					DBUtil.insert(conn, sql);
+
+					System.out.printf("%s님 회원가입이 완료되었습니다.\n", loginId);
 				}
 
 				if (cmd.equals("exit")) {
